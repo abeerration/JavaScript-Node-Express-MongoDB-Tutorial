97025da5031fa06a00aa6c819acdba9ee38059c1
abeerration <abeerration@gmail.com>
2024-10-11 01:15:31 +0500

    Update 3.loops.js

--- javascript/3.loops.js
+++ javascript/3.loops.js
@@ -1,3 +1,5 @@
+// while loop
+
 const list = ['first item', 'second item', 'third item']
 
 let i = 0
@@ -6,3 +8,18 @@ while (i < list.length) {
   console.log(i + ". " + list[i])
   i = i + 1
 }
+
+console.log()
+
+// do while loop
+
+
+i = 0
+
+do {
+  console.log(i + ". " + list[i])
+  i = i + 1
+} while (i < list.length)
+
+console.log()
+
