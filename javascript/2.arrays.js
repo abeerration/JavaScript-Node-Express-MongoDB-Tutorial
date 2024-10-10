56b3d9b8a2912879bda914c0c580627246e8e360
abeerration <abeerration@gmail.com>
2024-10-10 07:16:21 +0500

    Create 2.arrays.js

---
+++ javascript/2.arrays.js
@@ -0,0 +1,32 @@
+const a = []
+const b = Array()
+
+console.log(a)
+console.log(b)
+
+console.log()
+
+//
+
+const array1 = [1, 2, 3]
+const array2 = Array(1, 2, 3)
+const array3 = Array.of(1, 2, 3)
+
+console.log(array1)
+console.log(array2)
+console.log(array3)
+
+console.log()
+
+//
+
+const arr = [1, 'abeerration', ['1', 35, 'a']]
+
+console.log(arr[0])
+console.log(arr[1])
+console.log(`${arr[2][0]} ${arr[2][1]} ${arr[2][2]}`)
+
+console.log()
+
+//
+
